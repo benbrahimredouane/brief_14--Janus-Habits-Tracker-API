d539567a7e269ca8d44b3aecf6265204ea6a0136
redouane <benbrahimredouane664@gmail.com>
2026-03-13 10:47:50 +0000

    feat: modifiy UpdateHabitRequest to authorize user requests and refine validation rules

--- app/Http/Requests/UpdateHabitRequest.php
+++ app/Http/Requests/UpdateHabitRequest.php
@@ -2,28 +2,23 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateHabitRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'frequency' => 'sometimes|required|in:daily,weekly,monthly',
+            'target_days' => 'sometimes|required|integer|min:1',
+            'color' => 'sometimes|required|string|max:50',
         ];
     }
 }
