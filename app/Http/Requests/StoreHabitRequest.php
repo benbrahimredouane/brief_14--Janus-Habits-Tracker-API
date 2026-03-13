d0460406d24944f11e1e52f4b7591b40f448871c
redouane <benbrahimredouane664@gmail.com>
2026-03-13 10:38:25 +0000

    feat: update StoreHabitRequest to authorize user requests and refine validation rules

--- app/Http/Requests/StoreHabitRequest.php
+++ app/Http/Requests/StoreHabitRequest.php
@@ -2,28 +2,23 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreHabitRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'frequency' => 'required|in:daily,weekly,monthly',
+            'target_days' => 'required|integer|min:1',
+            'color' => 'required|string|max:50',
         ];
     }
 }
