2ab4e400f054e9b52b6d9baac38e0e2b0aeab5a5
redouane <benbrahimredouane664@gmail.com>
2026-03-13 10:48:19 +0000

    feat: create ApiFormRequest for handling validation and authorization

---
+++ app/Http/Requests/ApiFormRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class ApiFormRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator): void
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success' => false,
+                'data' => [
+                    'errors' => $validator->errors(),
+                ],
+                'message' => 'Validation failed.',
+            ], 422)
+        );
+    }
+}
